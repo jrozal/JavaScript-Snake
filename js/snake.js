347c39969fcdb55ef63e50669a68bc93d68d4f48
jrozal <68182930+jrozal@users.noreply.github.com>
2020-10-29 13:48:47 -0700

    Add update function to handle snake movement

--- js/snake.js
+++ js/snake.js
@@ -1,5 +1,18 @@
 export const SNAKE_SPEED = 1;
-const snakeBody = [{ x: 11, y: 11 }];
+const snakeBody = [
+  { x: 11, y: 11 },
+  { x: 12, y: 11 },
+  { x: 13, y: 11 }
+];
+
+export function update() {
+  for (let i = snakeBody.length - 2; i >= 0; i--) {
+    snakeBody[i + 1] = { ...snakeBody[i] }
+  }
+
+  // snakeBody[0].x += 1;
+  // snakeBody[0].y += 0;
+};
 
 export function draw(gameBoard) {
   snakeBody.forEach(segment => {
