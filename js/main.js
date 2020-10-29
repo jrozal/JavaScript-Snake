2a0c0b9d1ec023d6fd3b144462c601a7b97d0137
jrozal <68182930+jrozal@users.noreply.github.com>
2020-10-29 16:13:45 -0700

    Add food feature to update and render food pellets to game board

--- js/main.js
+++ js/main.js
@@ -1,17 +1,32 @@
+import { update as updateSnake, draw as drawSnake, SNAKE_SPEED } from './snake.js';
+import { update as updateFood, draw as drawFood } from './food.js';
 let lastRenderTime = 0;
+const gameBoard = document.getElementById('game-board');
 
 // function main takes in current time and renders game indefinitely
 function main(currentTime) {
   // request next frame to animate game
   window.requestAnimationFrame(main);
-
   const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
   if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
 
-  console.log('Render');
   // update render time
   lastRenderTime = currentTime;
+
+  update()
+  draw()
 };
 
 // init game render loop
 window.requestAnimationFrame(main);
+
+function update() {
+  updateSnake();
+  updateFood();
+};
+
+function draw() {
+  gameBoard.innerHTML = '';
+  drawSnake(gameBoard);
+  drawFood(gameBoard);
+};
